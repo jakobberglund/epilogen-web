46ba26fb9868adde4ecfea2a98b67f226bdb4980
jakobberglund <contact@jakobberglund.com>
2021-11-23 20:21:00 +0100

    add link to img

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,11 +1,17 @@
 <div>
-  <img
-    src="epilogen-den-tysta-massan-artwork.jpg"
-    width="1500"
-    height="1500"
-    loading="lazy"
-    alt="Den tysta massan artwork"
-  />
+  <a
+    href="https://epilogen.bandcamp.com/album/den-tysta-massan"
+    aria-label="Den tysta massan på Bandcamp"
+    class="cover-link"
+  >
+    <img
+      src="epilogen-den-tysta-massan-artwork.jpg"
+      width="1500"
+      height="1500"
+      loading="lazy"
+      alt="Den tysta massan artwork"
+    />
+  </a>
   <a href="https://epilogen.bandcamp.com/album/den-tysta-massan">Bandcamp</a>
   <a href="https://open.spotify.com/artist/6D2X3gxJG0yo8MiE2VtmvH?si=-mcydiRdTlaKT-z1GIoF3w"
     >Spotify</a
@@ -29,7 +35,6 @@
     height: auto;
     max-width: 700px;
     max-height: 700px;
-    margin-bottom: 1em;
   }
 
   a {
@@ -38,5 +43,15 @@
 
   a:hover {
     --epi--color--text: rgb(188, 56, 36);
+  }
+
+  .cover-link {
+    margin-bottom: 1em;
+    box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
+    transition: all 0.3s cubic-bezier(0.25, 0.8, 0.25, 1);
+  }
+
+  .cover-link:hover {
+    box-shadow: 0 10px 20px rgba(0, 0, 0, 0.19), 0 6px 6px rgba(0, 0, 0, 0.23);
   }
 </style>
