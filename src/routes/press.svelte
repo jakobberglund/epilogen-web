1a7f525bfb86c3678f3087d83251a3b3c70ae39c
jakobberglund <contact@jakobberglund.com>
2022-11-12 09:24:31 +0100

    update text

--- src/routes/press.svelte
+++ src/routes/press.svelte
@@ -12,8 +12,9 @@
     <p>
       Epilogen är Stockholmsduon Jakob Berglund på sång, synthar och maskiner, och Anders Carlström
       på gitarr och bas. Anders och Jakob är vänner sedan barndomen, och har länge spelat musik, var
-      för sig och tillsammans (främst i A Swarm of the Sun, där Jakob sjunger och skriver musik, och
-      Anders spelar bas).
+      för sig och tillsammans. Jakob är medlem i A Swarm of the Sun, där även Anders spelar bas live
+      och på skiva. Anders spelar även i The Isolation Process och Kausal, och var tidigare medlem i
+      Lingua.
     </p>
   </section>
   <section>
