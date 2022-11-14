19227f110e811e2ea7cb58dac5ea5c08607be410
jakobberglund <contact@jakobberglund.com>
2022-11-14 19:40:14 +0100

    add english route

---
+++ src/routes/en/press.svelte
@@ -0,0 +1,120 @@
+<article>
+  <h1>Press</h1>
+  <section>
+    <h2>Biography</h2>
+    <p>
+      On December 2, Epilogen will release its second album, Dekonstruktion. It is a development of
+      the debut album's mix of squealing guitars and rugged electronics. As the album title
+      suggests, it is it also a departure from clear structures: The songs are longer, textures take
+      up space, and maybe it's more uncompromising. Musically, it's a dark mix of rock, electronica
+      and rusty analog industrial soundscapes.
+    </p>
+    <p>
+      Epilogen is the Swedish duo Jakob Berglund on vocals, synths and alectronics, and Anders
+      Carlström on guitar and bass. Anders and Jakob have been friends since childhood, and have
+      been playing music for a long time separately and together. Jakob is a member of A Swarm of
+      the Sun, where Anders also plays bass live and on record. Anders is also a member of The
+      Isolation Process and Kausal.
+    </p>
+  </section>
+  <section>
+    <h2>Länkar</h2>
+    <p>
+      <a href="https://epilogen.bandcamp.com/album/dekonstruktion">Bandcamp</a>
+      <a href="https://open.spotify.com/artist/6D2X3gxJG0yo8MiE2VtmvH?si=-mcydiRdTlaKT-z1GIoF3w"
+        >Spotify</a
+      >
+      <a href="https://music.apple.com/us/artist/epilogen/1592826022">Apple Music</a>
+      <a href="https://www.facebook.com/epilogenmusik">Facebook</a>
+      <a href="https://www.instagram.com/epilogenmusik/">Instagram</a>
+      <a href="mailto:epilogenmusik@gmail.com">Email us</a>
+    </p>
+  </section>
+  <section>
+    <h2>Images</h2>
+    <h3>Dekonstruktion - Album cover</h3>
+    <p>Artwork by Epilogen. It is a cut collage of analog, self-developed photography.</p>
+    <figure>
+      <img
+        src="epilogen-dekonstruktion-artwork-1200x1200.jpg"
+        width="1200"
+        height="1200"
+        loading="lazy"
+        alt="Dekonstruktion artwork"
+      />
+    </figure>
+    <p>
+      Download
+      <a href="/epilogen-dekonstruktion-artwork-3000x3000.jpg" target="_blank">3000x3000</a>
+      <a href="/epilogen-dekonstruktion-artwork-1200x1200.jpg" target="_blank">1200x1200</a>
+    </p>
+    <h3>Band photography</h3>
+    <p>Photo: Johan Söderling and Jakob Berglund, 2022</p>
+    <figure>
+      <img
+        src="epilogen-dekonstruktion-bandfoto-1.jpg"
+        width="3000"
+        height="2000"
+        loading="lazy"
+        alt="Epilogen band photo"
+      />
+    </figure>
+    <p>
+      Download
+      <a href="epilogen-dekonstruktion-bandfoto-1.jpg" target="_blank">3000x2000</a>
+    </p>
+    <figure>
+      <img
+        src="epilogen-dekonstruktion-bandfoto-2.jpg"
+        width="3000"
+        height="2000"
+        loading="lazy"
+        alt="Epilogen band photo"
+      />
+    </figure>
+    <p>
+      Download
+      <a href="epilogen-dekonstruktion-bandfoto-2.jpg" target="_blank">3000x2000</a>
+    </p>
+  </section>
+</article>
+
+<style>
+  article {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 2rem;
+    background-color: black;
+    color: #ddd;
+    min-height: 100vh;
+  }
+
+  section {
+    width: 100%;
+    max-width: 700px;
+    text-align: start;
+  }
+
+  figure img {
+    border: 1px solid #222;
+    margin-bottom: 1em;
+  }
+
+  p:not(:last-child) {
+    margin-bottom: 1em;
+  }
+
+  img {
+    width: 100%;
+    height: auto;
+  }
+
+  a {
+    color: #5aaf9b;
+  }
+
+  h3 {
+    margin-bottom: 0.5em;
+  }
+</style>
